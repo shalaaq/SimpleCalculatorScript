fca7330440c9fe593f91a9f6e434694af624d361
Mahdi Yaghoubi <mahdideveloper06@gmail.com>
2024-02-06 16:46:47 +0330

    the func changed and we add a func for send sms

--- data.py
+++ data.py
@@ -1,5 +1,12 @@
-def calculator(a, b):
-    return a + b
-
-re = calculator(3, 5)
+def calculate(a, b):
+    J  = a + b
+    Z = a * b
+    return J, Z
+re = calculate(3, 5)
 print(re)
+
+
+def send_sms(number, sms):
+    print(f"{sms} was sent to {number}")
+    
+send_sms('0902', 'Fuckkkkkk Youuuuu!')
