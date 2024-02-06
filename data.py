6fcdc2033d7e917530b120d7f1a73b488cd0807d
Mahdi Yaghoubi <mahdideveloper06@gmail.com>
2024-02-06 17:17:52 +0330

    Add save sms to project

--- data.py
+++ data.py
@@ -10,3 +10,12 @@ def send_sms(number, sms):
     print(f"{sms} was sent to {number}")
     
 send_sms('0902', 'Fuckkkkkk Youuuuu!')
+
+
+
+def save_ph_number(*number):
+    phone_numbers = []
+    phone_numbers.append(number)
+    print(f'The list of phone numbers is {phone_numbers}')
+    
+save_ph_number('0912', '0991')
